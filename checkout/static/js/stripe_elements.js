cb6dfd59fc42ac1e725be1c2108cd98b78bc82de
alexeykuz-sys <alexeykuz@gmail.com>
2021-04-05 10:23:29 +0100

    fixed stripe bug fieldset

--- checkout/static/js/stripe_elements.js
+++ checkout/static/js/stripe_elements.js
@@ -7,11 +7,8 @@
 
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
-
-// Set up stripe and Elements
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
-
 var style = {
     base: {
         color: '#000',
@@ -23,19 +20,15 @@ var style = {
         }
     },
     invalid: {
-        color: '#dc3545', // Bootstrap 4 text-danger
+        color: '#dc3545',
         iconColor: '#dc3545'
     }
 };
-
-// Create an instance of the card Element.
 var card = elements.create('card', {style: style});
-
-// Add an instance of the card Element into the `card-element` <div>.
 card.mount('#card-element');
 
-// Handle real-time validation errors from the card Element.
-card.addEventListener('change', function(event) {
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
     if (event.error) {
         var html = `
@@ -43,23 +36,25 @@ card.addEventListener('change', function(event) {
                 <i class="fas fa-times"></i>
             </span>
             <span>${event.error.message}</span>
-        `
+        `;
         $(errorDiv).html(html);
     } else {
         errorDiv.textContent = '';
     }
 });
 
+// Handle form submit
 var form = document.getElementById('payment-form');
-form.addEventListener('submit', function (ev) {
+
+form.addEventListener('submit', function(ev) {
     ev.preventDefault();
-    card.update({ 'disabled': true });
+    card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
     $('#payment-form').fadeToggle(100);
     $('#loading-overlay').fadeToggle(100);
 
     var saveInfo = Boolean($('#id-save-info').attr('checked'));
-    // from {% csrf_token %} in the checkout form
+    // From using {% csrf_token %} in the form
     var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
     var postData = {
         'csrfmiddlewaretoken': csrfToken,
@@ -67,7 +62,7 @@ form.addEventListener('submit', function (ev) {
         'save_info': saveInfo,
     };
     var url = '/checkout/cache_checkout_data/';
-    
+
     $.post(url, postData).done(function () {
         stripe.confirmCardPayment(clientSecret, {
             payment_method: {
@@ -76,14 +71,14 @@ form.addEventListener('submit', function (ev) {
                     name: $.trim(form.full_name.value),
                     phone: $.trim(form.phone_number.value),
                     email: $.trim(form.email.value),
-                    address: {
+                    address:{
                         line1: $.trim(form.street_address1.value),
                         line2: $.trim(form.street_address2.value),
                         city: $.trim(form.town_or_city.value),
                         country: $.trim(form.country.value),
                         state: $.trim(form.county.value),
                     }
-                },
+                }
             },
             shipping: {
                 name: $.trim(form.full_name.value),
@@ -97,19 +92,18 @@ form.addEventListener('submit', function (ev) {
                     state: $.trim(form.county.value),
                 }
             },
-        }).then(function (result) {
+        }).then(function(result) {
             if (result.error) {
                 var errorDiv = document.getElementById('card-errors');
                 var html = `
                     <span class="icon" role="alert">
-                        <i class="fas fa-times"></i>
+                    <i class="fas fa-times"></i>
                     </span>
-                    <span>${result.error.message}</span>
-                `
+                    <span>${result.error.message}</span>`;
                 $(errorDiv).html(html);
                 $('#payment-form').fadeToggle(100);
                 $('#loading-overlay').fadeToggle(100);
-                card.update({ 'disabled': false });
+                card.update({ 'disabled': false});
                 $('#submit-button').attr('disabled', false);
             } else {
                 if (result.paymentIntent.status === 'succeeded') {
@@ -118,7 +112,7 @@ form.addEventListener('submit', function (ev) {
             }
         });
     }).fail(function () {
-        // Just reload the page, the error will be in Django messages
+        // just reload the page, the error will be in django messages
         location.reload();
-    });
+    })
 });
--- checkout/views.py
+++ checkout/views.py
@@ -34,11 +34,8 @@ def checkout(request):
 
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save(commit=False)
-            pid = request.POST.get('client_secret').split('_secret')[0]
-            order.original_bag = json.dumps(bag)
-            order.stripe_pid = pid
-            order.save()
+            order = order_form.save()
+           
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
