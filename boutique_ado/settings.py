90aaeb1c6da5346ddbeb5076aaaf2533aaa77226
alexeykuz-sys <alexeykuz@gmail.com>
2021-04-15 11:41:45 +0100

    deployment part 5 bugs

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -181,7 +181,7 @@ if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'alexey-boutique-ado'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_W3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     
     
@@ -195,7 +195,7 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 #Stripe
 FREE_DELIVERY_THRESHOLD = 50
